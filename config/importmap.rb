56e560b8a748a693a31d6fff5aed0e1dcbb24293
marcus allen <marcusal@marcuss-MacBook-Pro-2.local>
2025-02-02 20:25:26 +0000

    fix deployment

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,3 @@
 # Pin npm packages by running ./bin/importmap
 
 pin 'application'
-pin_all_from 'app/javascript/controllers', under: 'controllers'
-# ... existing code ...
-pin_all_from 'app/javascript/application', under: 'application'
